c84205bc52faad183fc151fa3e05e84e5a418e34
Thomas Renck <thomas.renck@epitech.eu>
2019-03-05 14:47:11 +0100

    adding my_itoa.c

---
+++ my_itoa.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2019
+** my_itoa.c
+** File description:
+** my_itoa
+*/
+
+char *my_itoa(int nb)
+{
+    int temp = nb;
+    int size = 0;
+    char *str;
+
+    while (temp != 0) {
+        temp = temp / 10;
+        size++;
+    }
+    str = malloc(sizeof(char) * (size + 1));
+    str[size] = 0;
+    while (size--) {
+        str[size] = nb % 10 + '0';
+        nb = nb / 10;
+    }
+    return (str);
+}
